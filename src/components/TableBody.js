bc09f2a90742c6673c51264680e046a792e438b6
JonatanQuiles87 <108682518+JonatanQuiles87@users.noreply.github.com>
2023-08-26 00:43:06 +0200

    TableRow and TableCell components have been created.

---
+++ src/components/TableBody.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import { useNavigate } from "react-router-dom";
+import {tableData} from "../data/tableData";
+import TableRow from "./TableRow";
+
+const TableBody = ({month}) => {
+    const gamesOfMonth = tableData.games.filter(game => game.date.match(month));
+    console.log("gamesOfMonth", gamesOfMonth);
+
+    return (
+        <tbody>
+        {
+            gamesOfMonth
+                .map(game => {
+                    return (
+                        <TableRow key={game.id} game={game}/>
+                    )
+                })
+        }
+        </tbody>
+    );
+};
+
+export default TableBody;
